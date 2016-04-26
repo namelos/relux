1bb14b9578d6d6e82382164f5027a43f584019fe
Yifan Wang <namelos@outlook.com>
2016-04-26 18:24:10 +0800

    readd

--- src/bindDynamicReducers.js
+++ src/bindDynamicReducers.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux'
 import createReducer from './createReducer'
 
 const defaultReducer = createReducer({}, {})
-
 export default function bindDynamicReducers(dynamicReducers) {
   return combineReducers({
     _: defaultReducer,
